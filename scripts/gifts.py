619acf6d75f909df19baa21a10ee0ed046cb53b6
9strebok <yastreb.ok.mag@gmail.com>
2020-10-16 23:18:06 +0300

    Refactoring is done

--- scripts/gifts.py
+++ scripts/gifts.py
@@ -11,11 +11,13 @@ import vk_api
 # Parser
 PARSER = argparse.ArgumentParser(description="Parse user gifts")
 
+
 # add python3 gifts.py [user_id_1] [user_id_2]
 PARSER.add_argument("users",
-                    nargs = "*",
-                    type  = int,
-                    help  = "python3 gifts.py [user_id_1] [user_id_2] [...]")
+                    nargs   = "*",
+                    type    = int,
+                    help    = "python3 gifts.py [user_id_1] [user_id_2] [...]")
+
 
 # add python3 gifts.py -x [max]
 PARSER.add_argument("-m",
@@ -25,39 +27,15 @@ PARSER.add_argument("-m",
                     default = 0,
                     help    = "python3 gifts.py -x [max]")
 
+
 args = PARSER.parse_args()
 
 
 class Colors():
     HEADER = '\033[95m'
-    # MAINCOLOR = '\033[0;36m'
     MAINCOLOR = '\033[33m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-
-
-def authorize():
-    try:
-        login = input(Colors.MAINCOLOR + Colors.BOLD + "[?] LOGIN: " + Colors.ENDC)
-        try:
-            passwd = getpass.getpass(Colors.MAINCOLOR + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
-        except:
-            print("[!] Error")
-            passwd = getpass.getpass(Colors.MAINCOLOR + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
-        api = vk_api.VkApi(login=login, password=passwd, session=None)
-        api.auth()
-        api = api.get_api()
-        return api
-    except vk_api.BadPassword as e:
-        print("[!] Bad password")
-
-
-def delete_vk_config():
-    if os.path.exists("vk_config.v2.json"):
-        os.system("rm vk_config.v2.json")
-    os.walk("..")
-    if os.path.exists("vk_config.v2.json"):
-        os.system("rm vk_config.v2.json")
 
 
 def color_string(string: str):
@@ -75,11 +53,42 @@ def nice_output_with_content(output: str, content):
     print(new_output, content)
 
 
+def authorize():
+    try:
+        login = input(color_string("[?] LOGIN: "))
+        try:
+            passwd = getpass.getpass(
+                                color_string("[?] PASSWORD: ")
+                            )
+        except:
+            print("[!] Error")
+            passwd = getpass.getpass(
+                                color_string("[?] PASSWORD: ")
+                            )
+
+        api = vk_api.VkApi( login = login,
+                            password = passwd,
+                            session = None )
+        api.auth()
+        api = api.get_api()
+        return api
+    except vk_api.BadPassword as e:
+        print("[!] Bad password")
+
+
+def delete_vk_config():
+    if os.path.exists("vk_config.v2.json"):
+        os.system("rm vk_config.v2.json")
+    os.walk("..")
+    if os.path.exists("vk_config.v2.json"):
+        os.system("rm vk_config.v2.json")
+
+
 def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
     try:
         print()
-        gifts_list = account.gifts.get(user_id=user)
-        account_info = account.users.get(user_ids=user)
+        gifts_list   = account.gifts.get(user_id  = user)
+        account_info = account.users.get(user_ids = user)
 
         # Create status bar
         bar_name = str(account_info[0].get("first_name") + " " + account_info[0].get("last_name"))
@@ -106,30 +115,35 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
                     tmp += 1
 
             if usr < 0 and tmp >= args.min:
-                usr = str(abs(usr))
-                public = "https://vk.com/public"
+
+                usr              = str(abs(usr))
+                public           = "https://vk.com/public"
                 public_plus_user = f"{public}{usr}"
-                output = color_string(f"\t\e]8;;{public_plus_user}\\a{public_plus_user}\e]8;;\\a\t")
-                out = f"echo '{output} {tmp}'"
+                output           = color_string(f"\t\e]8;;{public_plus_user}\\a{public_plus_user}\e]8;;\\a\t")
+                out              = f"echo '{output} {tmp}'"
+
                 os.system(out)
 
             elif usr > 0 and tmp >= args.min:
-                usr = str(usr)
-                vkid = "https://vk.com/id"
-                vkid_plus_usr = f"{vkid}{usr}"
-                output = color_string(f"\t\e]8;;{vkid_plus_usr}\\a{vkid_plus_usr}\e]8;;\\a\t")
-                out = f"echo '{output} {tmp}'"
+
+                usr              = str(usr)
+                vkid             = "https://vk.com/id"
+                vkid_plus_usr    = f"{vkid}{usr}"
+                output           = color_string(f"\t\e]8;;{vkid_plus_usr}\\a{vkid_plus_usr}\e]8;;\\a\t")
+                out              = f"echo '{output} {tmp}'"
+
                 os.system(out)
         print()
 
-
         groups_gifts = 0
         users_gifts = 0
+
         for i in gifts_list.get("items"):
             if i.get("from_id") < 0:
                 groups_gifts += 1
             elif i.get("from_id") > 0:
                 users_gifts += 1
+
         unidentified = gifts_list.get("count") - (groups_gifts + users_gifts)
 
         nice_output_with_content("Count:                  ", gifts_list.get("count"))
@@ -137,6 +151,7 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
         nice_output_with_content("Gifts from users:       ", users_gifts)
         nice_output_with_content("Unidentified gifts:     ", unidentified)
         print()
+
     except:
         print("Error, maybe you can't to get gifts list")
         print()
