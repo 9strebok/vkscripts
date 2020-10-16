9e6c9a85d13d20e11421a9b31bdadd94de37a694
9strebok <yastreb.ok.mag@gmail.com>
2020-10-16 23:03:07 +0300

    Recolor + Refactoring

--- scripts/gifts.py
+++ scripts/gifts.py
@@ -30,23 +30,20 @@ args = PARSER.parse_args()
 
 class Colors():
     HEADER = '\033[95m'
-    CYAN = '\033[0;36m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    # MAINCOLOR = '\033[0;36m'
+    MAINCOLOR = '\033[33m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def authorize():
     try:
-        login = input(Colors.CYAN + Colors.BOLD + "[?] LOGIN: " + Colors.ENDC)
+        login = input(Colors.MAINCOLOR + Colors.BOLD + "[?] LOGIN: " + Colors.ENDC)
         try:
-            passwd = getpass.getpass(Colors.CYAN + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
+            passwd = getpass.getpass(Colors.MAINCOLOR + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
         except:
             print("[!] Error")
-            passwd = getpass.getpass(Colors.CYAN + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
+            passwd = getpass.getpass(Colors.MAINCOLOR + Colors.BOLD + "[?] PASSWORD: " + Colors.ENDC)
         api = vk_api.VkApi(login=login, password=passwd, session=None)
         api.auth()
         api = api.get_api()
@@ -63,6 +60,21 @@ def delete_vk_config():
         os.system("rm vk_config.v2.json")
 
 
+def color_string(string: str):
+    new_string = f"{Colors.MAINCOLOR}{Colors.BOLD}{string}{Colors.ENDC}"
+    return new_string
+
+
+def nice_output(output: str):
+    new_output = f"{Colors.MAINCOLOR}{Colors.BOLD}{output}{Colors.ENDC}"
+    print(new_output)
+
+
+def nice_output_with_content(output: str, content):
+    new_output = f"{Colors.MAINCOLOR}{Colors.BOLD}\t{output}\t{Colors.ENDC}"
+    print(new_output, content)
+
+
 def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
     try:
         print()
@@ -70,8 +82,12 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
         account_info = account.users.get(user_ids=user)
 
         # Create status bar
-        bar_name = account_info[0].get("first_name") + " " + account_info[0].get("last_name")
-        bar = IncrementalBar(Colors.CYAN + Colors.BOLD + str(bar_name) + Colors.ENDC, max = len(gifts_list.get("items")))
+        bar_name = str(account_info[0].get("first_name") + " " + account_info[0].get("last_name"))
+
+        bar = IncrementalBar(
+                color_string(bar_name),
+                max = len(gifts_list.get("items"))
+        )
 
         usrs = []
         for i in gifts_list.get("items"):
@@ -80,7 +96,6 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
             bar.next()
             time.sleep(0.005)
         bar.finish()
-        # Drop status bar
         print()
 
         res = []
@@ -89,16 +104,22 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
             for i in gifts_list.get("items"):
                 if i.get("from_id") == usr:
                     tmp += 1
+
             if usr < 0 and tmp >= args.min:
                 usr = str(abs(usr))
                 public = "https://vk.com/public"
-                s = f"echo '\t{Colors.CYAN + Colors.BOLD}\e]8;;{public}{usr}\\a{public}{usr}\e]8;;\\a{Colors.ENDC}\t' {tmp}"
-                os.system(s)
+                public_plus_user = f"{public}{usr}"
+                output = color_string(f"\t\e]8;;{public_plus_user}\\a{public_plus_user}\e]8;;\\a\t")
+                out = f"echo '{output} {tmp}'"
+                os.system(out)
+
             elif usr > 0 and tmp >= args.min:
                 usr = str(usr)
                 vkid = "https://vk.com/id"
-                s = f"echo '\t{Colors.CYAN + Colors.BOLD}\e]8;;{vkid}{usr}\\a{vkid}{usr}\e]8;;\\a{Colors.ENDC}\t' {tmp}"
-                os.system(s)
+                vkid_plus_usr = f"{vkid}{usr}"
+                output = color_string(f"\t\e]8;;{vkid_plus_usr}\\a{vkid_plus_usr}\e]8;;\\a\t")
+                out = f"echo '{output} {tmp}'"
+                os.system(out)
         print()
 
 
@@ -110,10 +131,11 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
             elif i.get("from_id") > 0:
                 users_gifts += 1
         unidentified = gifts_list.get("count") - (groups_gifts + users_gifts)
-        print(Colors.CYAN + "\tCount:                  \t" + Colors.ENDC, gifts_list.get("count"))
-        print(Colors.CYAN + "\tGifts from groups:      \t" + Colors.ENDC, groups_gifts)
-        print(Colors.CYAN + "\tGifts from users:       \t" + Colors.ENDC, users_gifts)
-        print(Colors.CYAN + "\tUnidentified gifts:     \t" + Colors.ENDC, unidentified)
+
+        nice_output_with_content("Count:                  ", gifts_list.get("count"))
+        nice_output_with_content("Gifts from groups:      ", groups_gifts)
+        nice_output_with_content("Gifts from users:       ", users_gifts)
+        nice_output_with_content("Unidentified gifts:     ", unidentified)
         print()
     except:
         print("Error, maybe you can't to get gifts list")
@@ -121,7 +143,7 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod,user: int):
 
 
 def main():
-    BANNER = Colors.HEADER + Colors.BOLD + Colors.CYAN + """
+    BANNER = """
         .__  _____  __
    ____ |__|/ ____\/  |_  ______
   / ___\|  \   __\\\\   __\/  ___/
@@ -129,10 +151,12 @@ def main():
  \___  /|__||__|   |__| /____  >
 /_____/                      \/
 
-""" + Colors.ENDC
+"""
+    BANNER = color_string(BANNER)
 
     print(BANNER)
     account = authorize()
+
     for usr in args.users:
         get_gifts(account, usr)
 
