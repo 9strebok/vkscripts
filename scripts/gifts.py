a8d6032df12b8973e642527738432d821e2ffa01
9strebok <yastreb.ok.mag@gmail.com>
2020-11-01 02:29:16 +0300

    PEP8 formatting gifts.py

--- scripts/gifts.py
+++ scripts/gifts.py
@@ -13,18 +13,18 @@ PARSER = argparse.ArgumentParser(description="Parse user gifts")
 
 # add python3 gifts.py [user_id_1] [user_id_2]
 PARSER.add_argument("users",
-                    nargs   = "*",
-                    type    = int,
-                    help    = "python3 gifts.py [user_id_1] [user_id_2] [...]")
+                    nargs="*",
+                    type=int,
+                    help="python3 gifts.py [user_id_1] [user_id_2] [...]")
 
 
 # add python3 gifts.py -m [min]
 PARSER.add_argument("-m",
                     "--min",
-                    type    = int,
-                    dest    = "min",
-                    default = 0,
-                    help    = "python3 gifts.py -x [max]")
+                    type=int,
+                    dest="min",
+                    default=0,
+                    help="python3 gifts.py -x [max]")
 
 
 args = PARSER.parse_args()
@@ -36,16 +36,13 @@ class Colors():
     ENDC = '\033[0m'
     BOLD = '\033[1m'
 
-
     def color_string(string: str):
         new_string = f"{Colors.MAINCOLOR}{Colors.BOLD}{string}{Colors.ENDC}"
         return new_string
 
-
     def nice_output(output: str):
         new_output = f"{Colors.MAINCOLOR}{Colors.BOLD}{output}{Colors.ENDC}"
         print(new_output)
-
 
     def nice_output_with_content(output: str, content):
         new_output = f"{Colors.MAINCOLOR}{Colors.BOLD}\t{output}\t{Colors.ENDC}"
@@ -57,12 +54,13 @@ def authorize():
         login = input(Colors.color_string("[?] LOGIN: ")).strip()
         try:
             passwd = getpass.getpass(Colors.color_string("[?] PASSWORD: "))
-        except:
+        except Exception as e:
             print("[!] Error")
             passwd = getpass.getpass(Colors.color_string("[?] PASSWORD: "))
-        api = vk_api.VkApi( login = login,
-                            password = passwd,
-                            session = None )
+
+        api = vk_api.VkApi(login=login,
+                           password=passwd,
+                           session=None)
         api.auth()
         api = api.get_api()
         return api
@@ -82,17 +80,20 @@ def delete_vk_config():
 def get_gifts(account: vk_api.vk_api.VkApiMethod, user: int):
     try:
         print()
-        gifts_list   = account.gifts.get(user_id  = user)
-        account_info = account.users.get(user_ids = user)
+        gifts_list = account.gifts.get(user_id=user)
+        account_info = account.users.get(user_ids=user)
 
         count = len(gifts_list.get("items"))
 
         # Create status bar
-        bar_name = str(account_info[0].get("first_name") + " " + account_info[0].get("last_name"))
+        bar_name = str(
+            account_info[0].get("first_name") +
+            " " +
+            account_info[0].get("last_name"))
 
         bar = IncrementalBar(
-                Colors.color_string(bar_name),
-                max = count
+            Colors.color_string(bar_name),
+            max=count
         )
 
         usrs = []
@@ -113,7 +114,8 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod, user: int):
 
             res.append([usr, tmp])
 
-        res.sort(key = lambda k: k[1])
+        res.sort(key=lambda k: k[1])
+
         needed_users = reversed([r for r in res if r[1] >= args.min])
 
         groups_gifts = 0
@@ -129,7 +131,6 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod, user: int):
                 Colors.nice_output_with_content(url, usr[1])
                 groups_gifts += usr[1]
 
-
             elif usr[0] > 0:
                 usr_id = str(abs(usr[0]))
                 url = f"{user_url}{usr_id}"
@@ -139,10 +140,26 @@ def get_gifts(account: vk_api.vk_api.VkApiMethod, user: int):
         unidentified = count - (groups_gifts + users_gifts)
 
         print()
-        Colors.nice_output_with_content("Count:                  ", count)
-        Colors.nice_output_with_content("Gifts from groups:      ", groups_gifts)
-        Colors.nice_output_with_content("Gifts from users:       ", users_gifts)
-        Colors.nice_output_with_content("Unidentified gifts:     ", unidentified)
+
+        Colors.nice_output_with_content(
+                "Count:                  ",
+                count
+        )
+
+        Colors.nice_output_with_content(
+            "Gifts from groups:      ",
+            groups_gifts
+        )
+
+        Colors.nice_output_with_content(
+            "Gifts from users:       ",
+            users_gifts
+        )
+
+        Colors.nice_output_with_content(
+            "Unidentified gifts:     ",
+            unidentified
+        )
 
     except Exception as e:
         raise e
